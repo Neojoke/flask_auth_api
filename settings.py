866cd1e8f844dd5e8e91ed8fcff4e6e745280152
Neo <wupeng@touker.com>
2018-03-24 18:08:09 +0800

    init

---
+++ settings.py
@@ -0,0 +1,13 @@
+"""
+webconfig
+apiconfig
+"""
+####################
+# WEB CONFIG       #
+####################
+WEBPROT = 8122
+
+####################
+# DB CONFIG        #
+####################
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:wupeng90850@localhost:3306/Flask?charset=utf8mb4'
